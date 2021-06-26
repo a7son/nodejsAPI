4932894ea81850a9c972559c80567c6c069bd2c3
Asep Sonjaya <a7son_east110308@yahoo.com>
2021-06-26 22:10:30 +0700

    Getting Place by User id

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const placeRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.use(placeRoutes);
+app.use('/api/places',placeRoutes);
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({message: error.message || 'An unknown error occured!'});
+});
 
 app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,45 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famouse sky scrapers in the world',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: '20 W 35th St, New York, NY 10001',
+    creator: 'u1'
+  }
+]
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p=>{
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    return res.status(404).json({message: 'Could not find a place for the provided id.'});
+  }
+
   console.log('Get Request in Places');
-  res.json({ message: 'it works!' });
+  res.json({place});
 });
 
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find(p=>{
+    return p.creator === userId;
+  });  
+
+  if (!place) {
+    return res.status(404).json({message: 'Could not find a place for the provided id.'});
+  }
+  
+  res.json(place);
+});
 module.exports = router;
